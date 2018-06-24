d85ae9d389bd9bb4c7a1d018ff78822641c57374
Ivan <ivan.naumov92@yahoo.com>
2018-06-24 16:39:57 +0300

    Import

---
+++ MatrixTraining2.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace MatrixTraining2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var size = int.Parse(Console.ReadLine());
+
+            int[,] matrix = GetMatrix(size);
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    Console.Write(matrix[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        private static int[,] GetMatrix(int size)
+        {
+            var matrix = new int[size, size];
+
+            for (int i = 0, temp = 1; i < matrix.GetLength(0); i++, temp = i + 1)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++, temp+= 4)
+                {
+                    matrix[i, j] = temp;
+                }
+            }
+
+            return matrix;
+        }
+    }
+}
